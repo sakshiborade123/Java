8b650ff16b6d6ce5d9c17dda0c4e49afdae30e70
Sakshi Borade <boradesakshi494@gmail.com>
2026-01-06 22:23:56 +0530

    sum.java

---
+++ sum.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class sum{
+    public static void main (String[] args){
+
+        //Horizontally Print
+        System.out.print("Hello Sakshi ");
+        System.out.print('a');
+        System.out.print("Vikas");
+        System.out.print(55);
+        System.out.print(564.64);
+
+
+        //Vertically Print
+        System.out.println("HI GUYS WELCOME TO JAVA CLASS");
+        System.out.println(true);
+        System.out.println("Enter Number");
+        System.out.println(75);
+        System.out.println(62.05);
+        System.out.println('d');
+        System.out.println("Desai");
+        System.out.println(false);
+        System.out.println();
+
+
+
+
+        
+    }
+}
