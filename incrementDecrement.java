152737e5ad144638fefbd88376a3a91a8509e24e
Sakshi Borade <boradesakshi494@gmail.com>
2026-01-21 11:33:50 +0530

    Increment & Decrement

---
+++ incrementDecrement.java
@@ -0,0 +1,16 @@
+public class incrementDecrement {
+    public static void main(String[] args){
+        int p=-3, q= 8, r= 13, s;
+
+        s= p++ - q-- * r;
+        q= --p + s++ - --q;
+        p= p++ * p-- +r;
+        r= r--;
+
+        System.out.println(s);
+        System.out.println(q);
+        System.out.println(p);
+        System.out.println(r);
+    }
+    
+}
