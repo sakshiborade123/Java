8f5bd6a2df6e758ece2badf0160726bf807d64e5
Sakshi Borade <boradesakshi494@gmail.com>
2026-02-11 10:52:10 +0530

    Vovels

--- VovelConsonent.java
+++ VovelConsonent.java
@@ -1,15 +1,61 @@
 import java.util.Scanner;
-public class VovelConsonent {
-    public static void main(String[] args){
-        Scanner sc= new Scanner (System.in);
-        System.out.println("Enter the Character:");
 
-        char ch= sc.next().charAt(0);
+public class IncomeTax {
+    public static void main(String[] args) {
 
-        if(ch=='i' || ch=='a' || ch =='e' ||ch =='o' ||ch =='u'){
-            System.out.println("VOVEL");
-        }else{
-            System.out.println("CONSONENT");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your Salary:");
+        int salary = sc.nextInt();
+
+        System.out.println("Enter your Age:");
+        int age = sc.nextInt();
+
+     
+        if (age <= 60) {
+            if (salary <= 250000) {
+                System.out.println("Tax Rate = 0%");
+            } 
+            else if (salary <= 500000) {
+                System.out.println("Tax Rate = 5%");
+            } 
+            else if (salary <= 1000000) {
+                System.out.println("Tax Rate = 20%");
+            } 
+            else {
+                System.out.println("Tax Rate = 30%");
+            }
         }
+
+      
+        else if (age <= 80) {
+            if (salary <= 300000) {
+                System.out.println("Tax Rate = 0%");
+            } 
+            else if (salary <= 500000) {
+                System.out.println("Tax Rate = 5%");
+            } 
+            else if (salary <= 1000000) {
+                System.out.println("Tax Rate = 20%");
+            } 
+            else {
+                System.out.println("Tax Rate = 30%");
+            }
+        }
+
+      
+        else {
+            if (salary <= 500000) {
+                System.out.println("Tax Rate = 0%");
+            } 
+            else if (salary <= 1000000) {
+                System.out.println("Tax Rate = 20%");
+            } 
+            else {
+                System.out.println("Tax Rate = 30%");
+            }
+        }
+
+        sc.close();
     }
 }
