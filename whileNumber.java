54c45f6196bd9f1b5ff6558c8a2ff9505ac28682
Sakshi Borade <boradesakshi494@gmail.com>
2026-02-12 22:09:39 +0530

    WhileLoop

---
+++ whileNumber.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class whileNumber {
+    public static void main(String[] args){
+
+        int i=1;
+        while(i<100){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println(i);
+    }
+    
+}
---
+++ whileTables.java
@@ -0,0 +1,12 @@
+public class whileTables {
+    public static void main(String[] args){
+        int i=1;
+        int num=55;
+        while(i<=10){
+            System.out.println(i*num);
+            i++;
+        }
+        System.out.println(i);
+    }
+    
+}
