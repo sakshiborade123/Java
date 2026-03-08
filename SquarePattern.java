31367fd6f1a58ca6e66138dfdafd30847df672fd
Sakshi Borade <boradesakshi494@gmail.com>
2026-03-08 22:01:18 +0530

    Java Codes

---
+++ SquarePattern.java
@@ -0,0 +1,12 @@
+class SquarePattern{
+    public static void main(String[] args){
+        int num= 4;
+
+        for(int i= 0; i< num; i++){
+            for(int j=0; j<num; j++){
+                System.out.print("*"+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
