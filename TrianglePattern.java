c50b40a325f96b6bdb3009a8f30c8ba51e6e9f48
Sakshi Borade <boradesakshi494@gmail.com>
2026-03-11 20:16:11 +0530

    Java Code

---
+++ TrianglePattern.java
@@ -0,0 +1,14 @@
+class TrianglePattern{
+    public static void main(String[] args){
+        int n=7;
+        int count=0;
+        for(int i= 0;i<n; i++){
+            for(int j= 0;j <=i; j++){
+                count++;
+                System.out.print(count+"     ");
+                // System.out.print("* ");
+                
+            }System.out.println();
+    }
+        }
+}
