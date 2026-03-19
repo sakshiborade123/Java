e7a19ce83e9d7ee53995227532cd556df381cf6c
Sakshi Borade <boradesakshi494@gmail.com>
2026-03-19 21:48:41 +0530

    java codes

---
+++ method.java
@@ -0,0 +1,18 @@
+public class method{
+    public static void main(String[] args){
+        raju();
+        bheem();
+        chutki();
+    }
+
+    public static void raju(){
+        System.out.println("Hii Chutkii");
+    }
+    public static void bheem(){
+        System.out.println("Hi am there raju!!");
+    }
+    public static void chutki(){
+        System.out.println("GET Lost!!");
+    }
+    
+}
