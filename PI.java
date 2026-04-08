604eb0b66e8b9d580b93ce01b29383efc577a4f4
Sakshi Borade <boradesakshi494@gmail.com>
2026-04-08 20:58:04 +0530

    Java Code

---
+++ PI.java
@@ -0,0 +1,7 @@
+public class PI {
+    static double pi= 22.0/7.0;
+    public static void main(String[] args){
+        System.out.println(pi);
+    }
+    
+}
---
+++ PI1.java
@@ -0,0 +1,6 @@
+public class PI1 {
+    public static void main(String[] args){
+        System.out.println(PI.pi);
+    }
+    
+}
